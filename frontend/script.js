3561f12125c5b9b37ffad9a74b98d5b2f70572b7
vombat11 <gulyas.kata11@gmail.com>
2022-10-12 11:15:05 +0200

    js fetch

---
+++ frontend/script.js
@@ -0,0 +1,39 @@
+const apiKey = "pRufLGrUJbk85xKjA0BvXzNsDla9gACqzjlzY7c4";
+let apiDate = "2022-10-11";
+let fetchUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${apiDate}`;
+
+// fetch(fetchUrl).then(function(response) {
+//     return response.json()
+// }).then(function(responseJson) {
+//     console.log(responseJson)
+// });
+
+// fetch(fetchUrl)
+//     .then(response => response.json())
+//     .then(responseJson => console.log(responseJson));
+
+
+async function fetchNasa() {
+    const response = await fetch(fetchUrl)
+    // console.log(response)
+    const responseJson = await response.json()
+    // console.log(responseJson)
+    return responseJson
+};
+
+async function loadEvent() {
+    let data = await fetchNasa()
+    console.log("data: ", data)
+
+    const rootElement = document.querySelector("#root")
+    rootElement.insertAdjacentHTML("beforeend", `
+        <h1>${data.title}</h1>
+        <h2>${data.date}</h2>
+        <p>${data.explanation}</p>
+        <img src="${data.url}"/>
+    `)
+};
+
+window.addEventListener("load", loadEvent);
+
+
